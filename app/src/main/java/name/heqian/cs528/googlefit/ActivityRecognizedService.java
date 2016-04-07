fc918fe1d4a17d48e4cd4d256f558495eb059bb0
Benjamin Bianchi <bgbianchi@wpi.edu>
2016-04-06 20:21:05 -0400

    Toast is now showing.

--- app/src/main/java/name/heqian/cs528/googlefit/ActivityRecognizedService.java
+++ app/src/main/java/name/heqian/cs528/googlefit/ActivityRecognizedService.java
@@ -5,16 +5,26 @@ import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.location.ActivityRecognitionResult;
 import com.google.android.gms.location.DetectedActivity;
 
 import java.util.List;
+import android.os.Handler;
 
 /**
  * Created by Paul on 2/1/16.
  */
+
+
+
 public class ActivityRecognizedService extends IntentService {
+
+    static private int previousActivity = -2;
+    private int currentActivity = -1;
+    private Handler handler;
 
     public ActivityRecognizedService() {
         super("ActivityRecognizedService");
@@ -30,51 +40,123 @@ public class ActivityRecognizedService extends IntentService {
             ActivityRecognitionResult result = ActivityRecognitionResult.extractResult(intent);
             handleDetectedActivities( result.getProbableActivities() );
         }
+
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        handler = new Handler();
+        return super.onStartCommand(intent, flags, startId);
     }
 
     private void handleDetectedActivities(List<DetectedActivity> probableActivities) {
         for( DetectedActivity activity : probableActivities ) {
-            switch( activity.getType() ) {
+            switch (activity.getType()) {
                 case DetectedActivity.IN_VEHICLE: {
-                    Log.e( "ActivityRecogition", "In Vehicle: " + activity.getConfidence() );
+                    Log.e("ActivityRecogition", "In Vehicle: " + activity.getConfidence());
+                    if (activity.getConfidence() >= 75) {
+                        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+                        builder.setContentText("Are you in a Automobile?");
+                        builder.setSmallIcon(R.mipmap.ic_launcher);
+                        builder.setContentTitle(getString(R.string.app_name));
+                        NotificationManagerCompat.from(this).notify(0, builder.build());
+                        currentActivity = DetectedActivity.IN_VEHICLE;
+                    }
                     break;
                 }
                 case DetectedActivity.ON_BICYCLE: {
-                    Log.e( "ActivityRecogition", "On Bicycle: " + activity.getConfidence() );
+                    Log.e("ActivityRecogition", "On Bicycle: " + activity.getConfidence());
+                    if (activity.getConfidence() >= 75) {
+                        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+                        builder.setContentText("Are you biking?");
+                        builder.setSmallIcon(R.mipmap.ic_launcher);
+                        builder.setContentTitle(getString(R.string.app_name));
+                        NotificationManagerCompat.from(this).notify(0, builder.build());
+                        currentActivity = DetectedActivity.ON_BICYCLE;
+                    }
                     break;
                 }
                 case DetectedActivity.ON_FOOT: {
-                    Log.e( "ActivityRecogition", "On Foot: " + activity.getConfidence() );
+                    Log.e("ActivityRecogition", "On Foot: " + activity.getConfidence());
+                    if (activity.getConfidence() >= 75) {
+                        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+                        builder.setContentText("Are you on foot?");
+                        builder.setSmallIcon(R.mipmap.ic_launcher);
+                        builder.setContentTitle(getString(R.string.app_name));
+                        NotificationManagerCompat.from(this).notify(0, builder.build());
+                        currentActivity = DetectedActivity.ON_FOOT;
+                    }
                     break;
                 }
                 case DetectedActivity.RUNNING: {
-                    Log.e( "ActivityRecogition", "Running: " + activity.getConfidence() );
+                    Log.e("ActivityRecogition", "Running: " + activity.getConfidence());
+                    if (activity.getConfidence() >= 75) {
+                        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+                        builder.setContentText("Are you Running?");
+                        builder.setSmallIcon(R.mipmap.ic_launcher);
+                        builder.setContentTitle(getString(R.string.app_name));
+                        NotificationManagerCompat.from(this).notify(0, builder.build());
+                        currentActivity = DetectedActivity.RUNNING;
+                    }
                     break;
                 }
                 case DetectedActivity.STILL: {
-                    Log.e( "ActivityRecogition", "Still: " + activity.getConfidence() );
+                    Log.e("ActivityRecogition", "Still: " + activity.getConfidence());
+                    if (activity.getConfidence() >= 75) {
+                        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+                        builder.setContentText("Are you standing still?");
+                        builder.setSmallIcon(R.mipmap.ic_launcher);
+                        builder.setContentTitle(getString(R.string.app_name));
+                        NotificationManagerCompat.from(this).notify(0, builder.build());
+                        currentActivity = DetectedActivity.STILL;
+                    }
                     break;
                 }
                 case DetectedActivity.TILTING: {
-                    Log.e( "ActivityRecogition", "Tilting: " + activity.getConfidence() );
+                    Log.e("ActivityRecogition", "Tilting: " + activity.getConfidence());
+                    if (activity.getConfidence() >= 75) {
+                        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+                        builder.setContentText("Are you not upright?");
+                        builder.setSmallIcon(R.mipmap.ic_launcher);
+                        builder.setContentTitle(getString(R.string.app_name));
+                        NotificationManagerCompat.from(this).notify(0, builder.build());
+                        currentActivity = DetectedActivity.TILTING;
+                    }
                     break;
                 }
                 case DetectedActivity.WALKING: {
-                    Log.e( "ActivityRecogition", "Walking: " + activity.getConfidence() );
-                    if( activity.getConfidence() >= 75 ) {
+                    Log.e("ActivityRecogition", "Walking: " + activity.getConfidence());
+                    if (activity.getConfidence() >= 75) {
                         NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
-                        builder.setContentText( "Are you walking?" );
-                        builder.setSmallIcon( R.mipmap.ic_launcher );
-                        builder.setContentTitle( getString( R.string.app_name ) );
+                        builder.setContentText("Are you walking?");
+                        builder.setSmallIcon(R.mipmap.ic_launcher);
+                        builder.setContentTitle(getString(R.string.app_name));
                         NotificationManagerCompat.from(this).notify(0, builder.build());
+                        currentActivity = DetectedActivity.WALKING;
                     }
                     break;
                 }
                 case DetectedActivity.UNKNOWN: {
-                    Log.e( "ActivityRecogition", "Unknown: " + activity.getConfidence() );
+                    Log.e("ActivityRecogition", "Unknown: " + activity.getConfidence());
+                    currentActivity = DetectedActivity.UNKNOWN;
                     break;
                 }
             }
         }
+
+        if (currentActivity != previousActivity)
+            showToast("Changed Activity.");
+
+        previousActivity = currentActivity;
+    }
+    private void showToast(final String txt)
+    {
+        handler.post(new Runnable(){
+            @Override
+            public void run()
+            {
+                Toast.makeText(getApplicationContext(),txt,Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
