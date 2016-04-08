ec7ac30f1669aa6f567401163338cd5c7442c248
alonso_martinez <almartnz@gmail.com>
2016-04-07 22:16:07 -0400

    Commiting changes with time, toast

--- app/src/main/java/name/heqian/cs528/googlefit/ActivityRecognizedService.java
+++ app/src/main/java/name/heqian/cs528/googlefit/ActivityRecognizedService.java
@@ -8,6 +8,10 @@ import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.net.Uri;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import java.util.Calendar;
 
 import com.google.android.gms.location.ActivityRecognitionResult;
 import com.google.android.gms.location.DetectedActivity;
@@ -25,9 +29,13 @@ public class ActivityRecognizedService extends IntentService {
     public static final String PACKAGE_NAME = "name.heqian.cs528.googlefit";
     public static final String STRING_ACTION = PACKAGE_NAME + ".STRING_ACTION";
     public static final String STRING_EXTRA = PACKAGE_NAME + ".STRING_EXTRA";
-    static private int previousActivity = -2;
-    private int currentActivity = -1;
+    public static final String Mess = "OTHER";
+    static private int previousActivity = -1;
     private Handler handler;
+    private MediaPlayer mPlayer2 = new MediaPlayer();
+    long elapsedTime = 0;
+    Calendar c = Calendar.getInstance();
+    long startTime = System.currentTimeMillis();
 
     public ActivityRecognizedService() {
         super("ActivityRecognizedService");
@@ -41,9 +49,9 @@ public class ActivityRecognizedService extends IntentService {
     protected void onHandleIntent(Intent intent) {
         if(ActivityRecognitionResult.hasResult(intent)) {
             ActivityRecognitionResult result = ActivityRecognitionResult.extractResult(intent);
-            handleDetectedActivities( result.getProbableActivities() );
+            handleDetectedActivities(chooseBestResult(result.getProbableActivities()));
             Intent i = new Intent(STRING_ACTION);
-            i.putExtra(STRING_EXTRA, result.getMostProbableActivity().toString());
+            i.putExtra(STRING_EXTRA, chooseBestResult(result.getProbableActivities()).getType());
             LocalBroadcastManager.getInstance(this).sendBroadcast(i);
         }
 
@@ -55,105 +63,205 @@ public class ActivityRecognizedService extends IntentService {
         return super.onStartCommand(intent, flags, startId);
     }
 
-    private void handleDetectedActivities(List<DetectedActivity> probableActivities) {
-        for( DetectedActivity activity : probableActivities ) {
-            switch (activity.getType()) {
-                case DetectedActivity.IN_VEHICLE: {
-                    Log.e("ActivityRecogition", "In Vehicle: " + activity.getConfidence());
-                    if (activity.getConfidence() >= 75) {
-                        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
-                        builder.setContentText("Are you in a Automobile?");
-                        builder.setSmallIcon(R.mipmap.ic_launcher);
-                        builder.setContentTitle(getString(R.string.app_name));
-                        NotificationManagerCompat.from(this).notify(0, builder.build());
-                        currentActivity = DetectedActivity.IN_VEHICLE;
-                    }
-                    break;
-                }
-                case DetectedActivity.ON_BICYCLE: {
-                    Log.e("ActivityRecogition", "On Bicycle: " + activity.getConfidence());
-                    if (activity.getConfidence() >= 75) {
-                        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
-                        builder.setContentText("Are you biking?");
-                        builder.setSmallIcon(R.mipmap.ic_launcher);
-                        builder.setContentTitle(getString(R.string.app_name));
-                        NotificationManagerCompat.from(this).notify(0, builder.build());
-                        currentActivity = DetectedActivity.ON_BICYCLE;
-                    }
-                    break;
-                }
-                case DetectedActivity.ON_FOOT: {
-                    Log.e("ActivityRecogition", "On Foot: " + activity.getConfidence());
-                    if (activity.getConfidence() >= 75) {
-                        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
-                        builder.setContentText("Are you on foot?");
-                        builder.setSmallIcon(R.mipmap.ic_launcher);
-                        builder.setContentTitle(getString(R.string.app_name));
-                        NotificationManagerCompat.from(this).notify(0, builder.build());
-                        currentActivity = DetectedActivity.ON_FOOT;
-                    }
-                    break;
-                }
-                case DetectedActivity.RUNNING: {
-                    Log.e("ActivityRecogition", "Running: " + activity.getConfidence());
-                    if (activity.getConfidence() >= 75) {
-                        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
-                        builder.setContentText("Are you Running?");
-                        builder.setSmallIcon(R.mipmap.ic_launcher);
-                        builder.setContentTitle(getString(R.string.app_name));
-                        NotificationManagerCompat.from(this).notify(0, builder.build());
-                        currentActivity = DetectedActivity.RUNNING;
-                    }
-                    break;
-                }
-                case DetectedActivity.STILL: {
-                    Log.e("ActivityRecogition", "Still: " + activity.getConfidence());
-                    if (activity.getConfidence() >= 75) {
-                        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
-                        builder.setContentText("Are you standing still?");
-                        builder.setSmallIcon(R.mipmap.ic_launcher);
-                        builder.setContentTitle(getString(R.string.app_name));
-                        NotificationManagerCompat.from(this).notify(0, builder.build());
-                        currentActivity = DetectedActivity.STILL;
-                    }
-                    break;
-                }
-                case DetectedActivity.TILTING: {
-                    Log.e("ActivityRecogition", "Tilting: " + activity.getConfidence());
-                    if (activity.getConfidence() >= 75) {
-                        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
-                        builder.setContentText("Are you not upright?");
-                        builder.setSmallIcon(R.mipmap.ic_launcher);
-                        builder.setContentTitle(getString(R.string.app_name));
-                        NotificationManagerCompat.from(this).notify(0, builder.build());
-                        currentActivity = DetectedActivity.TILTING;
-                    }
-                    break;
-                }
-                case DetectedActivity.WALKING: {
-                    Log.e("ActivityRecogition", "Walking: " + activity.getConfidence());
-                    if (activity.getConfidence() >= 75) {
-                        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
-                        builder.setContentText("Are you walking?");
-                        builder.setSmallIcon(R.mipmap.ic_launcher);
-                        builder.setContentTitle(getString(R.string.app_name));
-                        NotificationManagerCompat.from(this).notify(0, builder.build());
-                        currentActivity = DetectedActivity.WALKING;
-                    }
-                    break;
-                }
-                case DetectedActivity.UNKNOWN: {
-                    Log.e("ActivityRecogition", "Unknown: " + activity.getConfidence());
-                    currentActivity = DetectedActivity.UNKNOWN;
-                    break;
+    private DetectedActivity chooseBestResult (List<DetectedActivity> probableActivities) {
+        int max = 0;
+        DetectedActivity best = null;
+        for (DetectedActivity act: probableActivities) {
+            if (act.getType() == DetectedActivity.IN_VEHICLE || act.getType() == DetectedActivity.RUNNING
+                    || act.getType() == DetectedActivity.WALKING || act.getType() == DetectedActivity.STILL) {
+                if (act.getConfidence() >= max) {
+                    max = act.getConfidence();
+                    best = act;
                 }
             }
         }
+        return best;
 
-        if (currentActivity != previousActivity)
-            showToast("Changed Activity.");
+    }
+    private void handleDetectedActivities(DetectedActivity activity) {
+//            System.out.println(activity);
+//        System.out.println("previous activity:"+ previousActivity);
+//            switch (activity.getType()) {
+//                case DetectedActivity.IN_VEHICLE: {
+//                    Log.e("ActivityRecogition", "In Vehicle: " + activity.getConfidence());
+//                    if (activity.getConfidence() >= 75) {
+//                        if(previousActivity!=activity.getType()) {
+//                            Log.d(Mess, "changing activity to vehicle");
+//                            startTime = System.currentTimeMillis();
+//                        }
+//                        mPlayer2.stop();
+//                        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+//                        builder.setContentText("Are you in a Automobile?");
+//                        builder.setSmallIcon(R.mipmap.ic_launcher);
+//                        builder.setContentTitle(getString(R.string.app_name));
+//                        NotificationManagerCompat.from(this).notify(0, builder.build());
+//                        //previousActivity = currentActivity;
+//
+//                        //currentActivity = DetectedActivity.IN_VEHICLE;
+//                        //System.out.println("Current Activity for Vehicle is: "+ currentActivity);
+//
+//                        System.out.println("Setting start time in vehicle");
+//
+//                    }
+//                    break;
+//                }
+//
+//
+//                case DetectedActivity.RUNNING: {
+//                    Log.e("ActivityRecogition", "Running: " + activity.getConfidence());
+//                    if (activity.getConfidence() >= 75) {
+//                        if(previousActivity!=activity.getType()) {
+//                            startTime = System.currentTimeMillis();
+//                            Log.d(Mess, "changing activity to running");
+//
+//                        }
+//                        System.out.println("Start Time Running: " + startTime);
+//                        playMedia();
+//                        System.out.println("PLaying media");
+//                        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+//                        builder.setContentText("Are you Running?");
+//                        builder.setSmallIcon(R.mipmap.ic_launcher);
+//                        builder.setContentTitle(getString(R.string.app_name));
+//                        NotificationManagerCompat.from(this).notify(0, builder.build());
+//                        //previousActivity = currentActivity;
+//
+////                        currentActivity = DetectedActivity.RUNNING;
+//                        //System.out.println("Current Activity for Runing is: "+ currentActivity);
+//
+//                        System.out.println("Setting start time in running");
+//
+//                    }
+//                    break;
+//                }
+//                case DetectedActivity.STILL: {
+//                    Log.e("ActivityRecogition", "Still: " + activity.getConfidence());
+//                    if (activity.getConfidence() >= 75) {
+//                        if(previousActivity!=activity.getType()) {
+//                            startTime = System.currentTimeMillis();
+//                            Log.d(Mess,"changing activity to still");
+//                        }
+//                        System.out.println("Start Time Still: " +startTime);
+//                        System.out.println("Elapsed Time Still: " + elapsedTime);
+//                        mPlayer2.stop();
+//                        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+//                        builder.setContentText("Are you standing still?");
+//                        builder.setSmallIcon(R.mipmap.ic_launcher);
+//                        builder.setContentTitle(getString(R.string.app_name));
+//                        NotificationManagerCompat.from(this).notify(0, builder.build());
+//                        //previousActivity = currentActivity;
+//
+////                        currentActivity = DetectedActivity.STILL;
+//                        //System.out.println("Current Activity for Still is: " +currentActivity);
+//
+//                        System.out.println("Setting start time in still");
+//
+//
+//                    }
+//                    break;
+//                }
+//
+//                case DetectedActivity.WALKING: {
+//                    Log.e("ActivityRecogition", "Walking: " + activity.getConfidence());
+//                    if (activity.getConfidence() >= 75) {
+//                        if(previousActivity!=activity.getType()) {
+//                            startTime = System.currentTimeMillis();
+//                            Log.d(Mess,"changing activity to walking");
+//                        }
+//                        System.out.println("Start Time Walking: " + startTime);
+//                        playMedia();
+//                        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+//                        builder.setContentText("Are you walking?");
+//                        builder.setSmallIcon(R.mipmap.ic_launcher);
+//                        builder.setContentTitle(getString(R.string.app_name));
+//                        NotificationManagerCompat.from(this).notify(0, builder.build());
+//                        //previousActivity = currentActivity;
+//                        //currentActivity = DetectedActivity.WALKING;
+//                        //System.out.println("Current Activity for Walking is: " +currentActivity);
+//                        System.out.println("Setting start time in walking");
+//
+//
+//                    }
+//                    break;
+//                }
+//
+//            }
 
-        previousActivity = currentActivity;
+//        if (previousActivity != activity.getType()) {
+//
+//           System.out.println("DIFFERENT");
+//            System.out.println("DIFFERENT");
+//            System.out.println("DIFFERENT");
+//            System.out.println("DIFFERENT");
+//            System.out.println("DIFFERENT");
+//            System.out.println("DIFFERENT");
+//            System.out.println("DIFFERENT");
+//            System.out.println("DIFFERENT");
+//            System.out.println("DIFFERENT");
+//            System.out.println("DIFFERENT");
+//
+//            long currentTime = System.currentTimeMillis();
+//
+//            System.out.println("Current Time is: " + currentTime);
+//            System.out.println("Start Time is: " + startTime);
+//
+//            elapsedTime = currentTime - startTime;
+//            System.out.println("Elapsed Time After 1000: "+elapsedTime);
+//            int minutes = 0;
+//            int seconds = 0;
+//
+//            while (elapsedTime > 60) {
+//                elapsedTime = elapsedTime - 60;
+//                minutes++;
+//            }
+//
+//            seconds = (int) elapsedTime;
+//
+//            String time = Integer.toString(minutes) + "min, " + Integer.toString(seconds) + " seconds.";
+//
+//            if(previousActivity >= 0) {
+//                String text = "";
+//
+//                switch (previousActivity) {
+//
+//                    case 0:
+//                        text = "You were in a vehicle for ";
+//                        break;
+//
+//                    case 1:
+//                        text = "You were on a bike for ";
+//                        break;
+//
+//                    case 2:
+//                        text = "You were on foot for ";
+//                        break;
+//
+//                    case 3:
+//                        text = "You were still for ";
+//                        break;
+//
+//                    case 4:
+//                        text = "You were unknown for ";
+//                        break;
+//
+//                    case 5:
+//                        text = "You were tilting for ";
+//                        break;
+//
+//                    case 7:
+//                        text = "You were walking for ";
+//                        break;
+//
+//                    case 8:
+//                        text = "You were running for ";
+//                        break;
+//
+//                }
+//
+//
+//                showToast(text + time);
+//            }
+//            previousActivity = activity.getType();
+//        }
     }
     private void showToast(final String txt)
     {
@@ -164,5 +272,21 @@ public class ActivityRecognizedService extends IntentService {
                 Toast.makeText(getApplicationContext(),txt,Toast.LENGTH_LONG).show();
             }
         });
+    }
+
+    public void playMedia(){
+
+        try {
+            //mPlayer2.reset();
+            mPlayer2.setDataSource(this, Uri.parse("android.resource://name.heqian.cs528.googlefit/drawable/beat_02"));
+            mPlayer2.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            mPlayer2.setLooping(false);
+            mPlayer2.prepare();
+            mPlayer2.start();
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/name/heqian/cs528/googlefit/MainActivity.java
+++ app/src/main/java/name/heqian/cs528/googlefit/MainActivity.java
@@ -9,7 +9,11 @@ import android.content.IntentFilter;
 import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
@@ -17,6 +21,7 @@ import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -47,6 +52,10 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
     private InfoDBHandler dbHandler;
     public static ImageView imgView;
     private ActivityDetectionBroadcastReceiver mBroadcastReceiver;
+    private static long prevTime = 0;
+    private Handler handler;
+    private MediaPlayer mPlayer2 = new MediaPlayer();
+    private static int previous = -2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,18 +92,50 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
         //imgView.setImageResource(R.drawable.in_vehicle);
         mApiClient.connect();
     }
-    public int getDetectedActivity(String detectedActivityType) {
-        Log.d("MESS", "Recieved activity string "+detectedActivityType);
-        if (detectedActivityType.contains("VEHICLE")) {
-            return R.drawable.in_vehicle;
-        } else if (detectedActivityType.contains("RUNNING")) {
-            return R.drawable.running;
-        } else if (detectedActivityType.contains("WALKING")) {
-            return R.drawable.walking;
-        } else if (detectedActivityType.contains("STILL")) {
-            return R.drawable.still;
-        } else {
-            return 0;
+
+    public int getDetectedActivity(int detectedActivityType) {
+        Log.d("MESS", "Recieved activity string " + detectedActivityType);
+
+
+        if (previous != detectedActivityType) {
+            long currentTime = System.currentTimeMillis();
+            Log.d("MESS", "Changed from " + previous + " after " + (currentTime - prevTime) + " ms");
+            long totalTime = ((currentTime - prevTime) / 1000);
+            long minutes = totalTime/60;
+            long seconds = totalTime%60;
+            showToast(getName(previous) + Long.toString(minutes) +" mins, " +seconds + " secs.");
+            Log.d("MESS", "total time is " + totalTime + " ms");
+
+            prevTime = currentTime;
+            previous = detectedActivityType;
+
+        }
+
+
+//
+//        if (detectedActivityType.contains("VEHICLE")) {
+//            return R.drawable.in_vehicle;
+//        } else if (detectedActivityType.contains("RUNNING")) {
+//            return R.drawable.running;
+//        } else if (detectedActivityType.contains("WALKING")) {
+//            return R.drawable.walking;
+//        } else if (detectedActivityType.contains("STILL")) {
+//            return R.drawable.still;
+//        } else {
+//            return 0;
+//        }
+
+        switch (detectedActivityType) {
+            case DetectedActivity.IN_VEHICLE:
+                return R.drawable.in_vehicle;
+            case DetectedActivity.RUNNING:
+                return R.drawable.running;
+            case DetectedActivity.WALKING:
+                return R.drawable.walking;
+            case DetectedActivity.STILL:
+                return R.drawable.still;
+            default:
+                return -1;
         }
     }
 
@@ -103,7 +144,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
         Location mLastLocation = null;
         Intent intent = new Intent(this, ActivityRecognizedService.class);
         PendingIntent pendingIntent = PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-        ActivityRecognition.ActivityRecognitionApi.requestActivityUpdates(mApiClient, 0, pendingIntent);
+        ActivityRecognition.ActivityRecognitionApi.requestActivityUpdates(mApiClient, 100, pendingIntent);
         if ((this.getApplicationContext().checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION))
                 == PackageManager.PERMISSION_GRANTED) {
             mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
@@ -142,6 +183,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -153,6 +195,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
         LocalBroadcastManager.getInstance(this).unregisterReceiver(mBroadcastReceiver);
         super.onPause();
     }
+
     @Override
     public void onLocationChanged(Location location) {
         if (location != null) {
@@ -172,20 +215,97 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
         mMap.animateCamera(CameraUpdateFactory
                 .newCameraPosition(cameraPosition));
     }
+
     public class ActivityDetectionBroadcastReceiver extends BroadcastReceiver {
 
         @Override
         public void onReceive(Context context, Intent intent) {
-            Log.d("MESS", "Recieved something "+intent.getStringExtra(STRING_EXTRA));
-            String detectedAct = intent.getStringExtra(STRING_EXTRA);
+            Log.d("MESS", "Recieved something " + intent.getStringExtra(STRING_EXTRA));
+            int detectedAct = intent.getIntExtra(STRING_EXTRA, -1);
             int imageRes = getDetectedActivity(detectedAct);
-            Log.d("MESS", "Image resource is "+imageRes);
-            if (imageRes != 0) {
+            Log.d("MESS", "Image resource is " + imageRes);
+            if (imageRes != -1) {
                 imgView.setImageResource(imageRes);
             } else {
-                imgView.setImageResource(R.drawable.running);
+                imgView.setImageResource(R.drawable.startbutton);
             }
 
         }
     }
-}
+
+
+    private void showToast(final String txt) {
+                Toast.makeText(getApplicationContext(), txt, Toast.LENGTH_LONG).show();
+
+    }
+
+    public void playMedia() {
+
+        try {
+            //mPlayer2.reset();
+            mPlayer2.setDataSource(this, Uri.parse("android.resource://name.heqian.cs528.googlefit/drawable/beat_02"));
+            mPlayer2.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            mPlayer2.setLooping(false);
+            mPlayer2.prepare();
+            mPlayer2.start();
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+    }
+
+
+    public String getName(int type) {
+
+         String text = "";
+
+            switch (type) {
+
+                case 0:
+                    text = "You were in a vehicle for ";
+                    break;
+
+                case 1:
+                    text = "You were on a bike for ";
+
+                break;
+
+                case 2:
+                    text = "You were on foot for ";
+
+                break;
+
+                case 3:
+                    text = "You were still for ";
+
+                break;
+
+                case 4:
+                    text = "You were unknown for ";
+
+                break;
+
+                case 5:
+                    text = "You were tilting for ";
+
+                break;
+
+                case 7:
+                    text = "You were walking for ";
+
+                break;
+
+                case 8:
+                    text = "You were running for ";
+
+                break;
+
+                default:
+                    text = "error";
+
+            }
+            return text;
+        }
+    }
